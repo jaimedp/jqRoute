a81c91679c559e57b414c2215dbb8ee9af715f09
Jaime del Palacio <jpalacio@aklara.com>
2011-09-14 13:57:37 -0500

    removed the line numbers from the js file that were included by mistake

--- appBase.js
+++ appBase.js
@@ -1,66 +1,66 @@
  /*---------------------------------------------------------*/
-   2: /* The routing strategy */
-   3: (function (app, $) {
-   4:     var st = {
-   5:         strategies: {
-   6:             mvc: {
-   7:                 defaultController: 'home',
-   8:                 defaultAction: 'index',
-   9:                 controller: function controllerFromUrl() {
-  10:                     // match urls in the form ../controllerName/actionName or ../controllerName
-  11:                     var ctl = document.location.href.match(/http[s]?:\/\/[\w\:\.\-\d]+\/([\w\d\-]+)[\/|$]/i);
-  12:                     return ctl ? ctl[1].toLowerCase() : this.defaultController;
-  13:                 },
-  14:                 action: function actionFromUrl() {
-  15:                     // match urls in the form ../controllerName/actionName or ../controllerName
-  16:                     var act = document.location.href.match(/http[s]?:\/\/[\w\:\.\-\d]+\/[\w\d\-]+\/([\w\d\-]+)/i);
-  17:                     return act ? act[1].toLowerCase() : this.defaultAction;
-  18:                 }
-  19:             }
-  20:         }
-  21:     };
-  22:  
-  23:     window.myApplication = $.extend(app, st);
-  24: })(window.myApplication || {}, jQuery);
-  25:  
-  26:  
-  27:  
-  28: /*--------------------------------------------*/
-  29: /* The basic routing object */
-  30:  
-  31: (function (app, $) {
-  32:     var opts = {
-  33:         routingStrategy: app.strategies.mvc
-  34:     };
-  35:  
-  36:     var theApp = {
-  37:         activeController: undefined,
-  38:         controllers: {},
-  39:         run: function () {
-  40:             this.initController();
-  41:             this.initAction();
-  42:         },
-  43:         initController: function () {
-  44:             var activeControllerName =  opts.routingStrategy.controller(); /*getCurrentControllerName*/
-  45:             if (activeControllerName && this.controllers[activeControllerName]) {
-  46:                 this.activeController = new this.controllers[activeControllerName]();
-  47:                 this.activeController.init();
-  48:             }
-  49:         },
-  50:         initAction: function () {
-  51:             var activeActionName = opts.routingStrategy.action(); /*getCurrentActionName*/
-  52:             if (activeActionName && this.activeController && this.activeController[activeActionName]) {
-  53:                  this.activeController[activeActionName]();
-  54:             }
-  55:         }
-  56:     };
-  57:  
-  58:     /* pusblish the routing object */
-  59:     window.myApplication = $.extend(app, theApp);
-  60:  
-  61: })(window.myApplication || {}, jQuery);
-  62:  
-  63:  
-  64: $(function () {
-  65:     myApplication.run();
-  66: });
+ /* The routing strategy */
+ (function (app, $) {
+     var st = {
+         strategies: {
+             mvc: {
+                 defaultController: 'home',
+                 defaultAction: 'index',
+                 controller: function controllerFromUrl() {
+                     // match urls in the form ../controllerName/actionName or ../controllerName
+                     var ctl = document.location.href.match(/http[s]?:\/\/[\w\:\.\-\d]+\/([\w\d\-]+)[\/|$]/i);
+                     return ctl ? ctl[1].toLowerCase() : this.defaultController;
+                 },
+                 action: function actionFromUrl() {
+                     // match urls in the form ../controllerName/actionName or ../controllerName
+                     var act = document.location.href.match(/http[s]?:\/\/[\w\:\.\-\d]+\/[\w\d\-]+\/([\w\d\-]+)/i);
+                     return act ? act[1].toLowerCase() : this.defaultAction;
+                 }
+             }
+         }
+     };
+  
+     window.myApplication = $.extend(app, st);
+ })(window.myApplication || {}, jQuery);
+  
+  
+  
+ /*--------------------------------------------*/
+ /* The basic routing object */
+  
+ (function (app, $) {
+     var opts = {
+         routingStrategy: app.strategies.mvc
+     };
+  
+     var theApp = {
+         activeController: undefined,
+         controllers: {},
+         run: function () {
+             this.initController();
+             this.initAction();
+         },
+         initController: function () {
+             var activeControllerName =  opts.routingStrategy.controller(); /*getCurrentControllerName*/
+             if (activeControllerName && this.controllers[activeControllerName]) {
+                 this.activeController = new this.controllers[activeControllerName]();
+                 this.activeController.init();
+             }
+         },
+         initAction: function () {
+             var activeActionName = opts.routingStrategy.action(); /*getCurrentActionName*/
+             if (activeActionName && this.activeController && this.activeController[activeActionName]) {
+                  this.activeController[activeActionName]();
+             }
+         }
+     };
+  
+     /* pusblish the routing object */
+     window.myApplication = $.extend(app, theApp);
+  
+ })(window.myApplication || {}, jQuery);
+  
+  
+ $(function () {
+     myApplication.run();
+ });
